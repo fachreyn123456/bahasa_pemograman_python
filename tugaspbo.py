f799167a1ecbfbb0daf73b33d05eee98ffe1e62d
fachreyn123456 <fachreyn@gmail.com>
2022-10-05 22:36:49 +0700

    update

---
+++ tugaspbo.py
@@ -0,0 +1,26 @@
+print("PROGRAM MENGHITUNG NILAI MAHASISWA")
+
+nama=input("Masukan Nama :")
+nim=input("Masukan Nim :")
+jk=input("Masukan Jenis Kelamin :")
+tgl=input("Masukan Tanggal Input Nilai :")
+matkul=input("Masukan Mata Kuliah :")
+absensi=input("Masukan Absensi :")
+tugas=float(input("Masukan Nilai Tugas :"))
+uts=float(input("Masukan Nilai UTS :"))
+uas=float(input("Masukan Nilai UAS :"))
+
+na=(tugas * 0.2) + (uts * 0.4) + (uas * 0.4)
+
+print("HASIL PERHITUNGAN")
+
+print("Nama :" ,nama)
+print("Nim :" ,nim)
+print("Jenis Kelamin :" ,jk)
+print("Tanggal Input Nilai :" ,tgl)
+print("Mata Kuliah :" ,matkul)
+print("Absensi :" ,absensi)
+print("Nilai Tugas :" ,tugas)
+print("Nilai Uts :" ,uts)
+print("Nilai Uas :" ,uas)
+print("Nilai Akhir :" ,na)
